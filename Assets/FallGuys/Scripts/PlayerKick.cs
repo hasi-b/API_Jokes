f38820d7b0184be22c6a9c416db73adf0852cd62
Hasi-b <hasib.hasan9@gmail.com>
2023-09-28 01:52:12 +0600

    added player Kick functionality

--- Assets/FallGuys/Scripts/PlayerKick.cs
+++ Assets/FallGuys/Scripts/PlayerKick.cs
@@ -2,27 +2,39 @@ using UnityEngine;
 
 public class PlayerKick : MonoBehaviour
 {
-    public float kickForce = 10f;          // The force of the kick.
-    public float kickRange = 2f;           // The range of the kick.
-    public KeyCode kickKey = KeyCode.F;    // The key to trigger the kick.
+    [SerializeField] 
+    float kickForce = 10f;          // The force of the kick.
+    [SerializeField]  
+    float kickRange = 2f;           // The range of the kick.
+    [SerializeField]
+    KeyCode kickKey = KeyCode.F;
+
+    #region
+    Ray ray;
+    RaycastHit hit;
+    Color rayColor = Color.red;
+    Rigidbody hitRigidbody;
+    #endregion
+    // The key to trigger the kick.
 
     void Update()
     {
         // Check if the kick key is pressed.
-       
+        if (Input.GetKeyDown(kickKey))
+        {
             // Perform the kick action.
             PerformKick();
-        
+        }
     }
 
     void PerformKick()
     {
         // Create a ray from the player's position in the forward direction.
-        Ray ray = new Ray(transform.position + new Vector3(0f,0.5f,0f), transform.forward);
-        RaycastHit hit;
+        ray = new Ray(transform.position + new Vector3(0f,1f,0f), transform.forward);
+        
 
         // Set the default color to red.
-        Color rayColor = Color.red;
+        
 
         // Cast the ray and check if it hits an object within the kick range.
         if (Physics.Raycast(ray, out hit, kickRange))
@@ -31,15 +43,15 @@ public class PlayerKick : MonoBehaviour
             rayColor = Color.green;
 
             // Check if the hit object has a Rigidbody component.
-            Rigidbody hitRigidbody = hit.collider.GetComponent<Rigidbody>();
-            if (hitRigidbody != null && (Input.GetKeyDown(kickKey)))
+            hitRigidbody = hit.collider.GetComponent<Rigidbody>();
+            if (hitRigidbody != null)
             {
                 // Apply a force to the hit object in the direction of the ray.
                 hitRigidbody.AddForce(ray.direction * kickForce, ForceMode.Impulse);
             }
         }
 
-        // Draw the ray with the determined color to make it visible in the scene view.
-        Debug.DrawRay(ray.origin, ray.direction * kickRange, rayColor, 0.5f);
+        // Draw the ray with the determined color and thickness.
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * kickRange, rayColor, 0.5f);
     }
 }
